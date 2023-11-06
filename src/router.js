f4dbe27d02d32b4386b263ff843a8204fbb1b1a7
hodhodi <amis883@yahoo.com>
2023-11-06 10:58:55 +0330

    create showuser

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import IndexUser from './pages/users/IndexUser.vue'
-
-
+import ShowUser from './pages/users/ShowUser.vue'
 import HomePage from './pages/HomePage.vue'
+
+
 const routes = [{ path: "/", name: "home", component: HomePage },
-                { path: "/users", name: "users", component: IndexUser }]
+    { path: "/users", name: "users", component: IndexUser },
+    {  path: "/users/:id", name: 'userId', component: ShowUser }
+]
+
 const router = createRouter({
     history: createWebHistory(),
     routes
